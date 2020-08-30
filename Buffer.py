f2dc20c338f5033162d0d581650ea844e09fa6bd
EJ <ethanjameswatkins@gmail.com>
2020-08-30 14:48:34 +1000

    class AcidBase

--- Buffer.py
+++ Buffer.py
@@ -1,20 +1,26 @@
 import math
 from decimal import *
 
-initial_conc = [0, 0.0246, 0.0754, 0]
-pKa = [2.15, 6.86, 12.32]
+class AcidBase:
+    def __init__(self, species_names, initial_conc, pKa):
+        self.species_names = species_names
+        self.initial_conc = initial_conc
+        self.pKa = pKa
+
+phosphoric = AcidBase(["H3PO4", "H2PO4 -", "HPO4 2-", "PO4 3-"], [0, 0.0246, 0.0754, 0], [2.15, 6.86, 12.32])
+
 #pKa = [2.14, 7.20, 12.37]
 
-def release(pH_guess, species_names, initial_conc, pKa, protons, printer = 0):
+def release(pH_guess, acidbase, protons, printer = 0):
     total_conc = 0
-    for i in initial_conc:
+    for i in acidbase.initial_conc:
         total_conc += i
     if printer == 1:
         print("total conc", total_conc)
 
     base_acid = []
-    for i in range(len(initial_conc) - 1):
-        base_acid.append(10**(pH_guess - pKa[i]))
+    for i in range(len(acidbase.initial_conc) - 1):
+        base_acid.append(10**(pH_guess - acidbase.pKa[i]))
     if printer == 1:
         print("base_acid", base_acid)
 
@@ -37,8 +43,8 @@ def release(pH_guess, species_names, initial_conc, pKa, protons, printer = 0):
         print("equilibrium conc", equilibrium_conc)
 
     diff_conc = []
-    for i in range(len(initial_conc)):
-        diff_conc.append(equilibrium_conc[i] - initial_conc[i])
+    for i in range(len(acidbase.initial_conc)):
+        diff_conc.append(equilibrium_conc[i] - acidbase.initial_conc[i])
     if printer == 1:
         print("diff conc", diff_conc)
 
@@ -58,49 +64,51 @@ def pH_calc(H_released, printer = 0):
         print("pH_calc", pH)
     return float(pH)
 
-def diff(pH_guess):
-    H_release = release(pH_guess, ["H3PO4", "H2PO4 -", "HPO4 2-", "PO4 3-"], initial_conc, pKa, [3, 2, 1, 0])
+def diff(pH_guess, acidBases):
+    H_release = 0
+    for acidBase in acidBases:
+        H_release += release(pH_guess, acidBase, [3, 2, 1, 0])
     pH_calculated = pH_calc(H_release)
     return pH_guess - pH_calculated
 
-def search(precision):
+def search(precision, acidBases):
     lowerbound = 2
     upperbound = 14
 
-    while diff(lowerbound) > 0:
+    while diff(lowerbound, acidBases) > 0:
         lowerbound += -1
 
-    while diff(upperbound) < 0:
+    while diff(upperbound, acidBases) < 0:
         upperbound += 1
 
-    diff_lower = diff(lowerbound)
-    diff_upper = diff(upperbound)
+    diff_lower = diff(lowerbound, acidBases)
+    diff_upper = diff(upperbound, acidBases)
     gradient = (diff_upper - diff_lower) / (upperbound - lowerbound)
     guess = upperbound - (diff_upper / gradient)
-    diff_guess = diff(guess)
+    diff_guess = diff(guess, acidBases)
 
     while not Decimal(-precision) < diff_guess < Decimal(precision):
         if diff_guess < 0:
             lowerbound = guess
-            diff_lower = diff(lowerbound)
+            diff_lower = diff(lowerbound, acidBases)
         else:
             upperbound = guess
-            diff_upper = diff(upperbound)
+            diff_upper = diff(upperbound, acidBases)
 
         gradient = (diff_upper - diff_lower) / (upperbound - lowerbound)
         guess = upperbound - (diff_upper / gradient)
-        diff_guess = diff(guess)
+        diff_guess = diff(guess, acidBases)
 
     print("Found")
     print("guess", guess)
     print("diff guess", diff_guess)
-    H_release = release(guess, ["H3PO4", "H2PO4 -", "HPO4 2-", "PO4 3-"], initial_conc, pKa, [3, 2, 1, 0], 1)
-    print("pH calc", pH_calc(H_release))
+#    H_release = release(guess, ["H3PO4", "H2PO4 -", "HPO4 2-", "PO4 3-"], initial_conc, pKa, [3, 2, 1, 0], 1)
+#    print("pH calc", pH_calc(H_release))
 
 
 #pH_guess = 5
 
-search(0.00001)
+search(0.00001, [phosphoric])
 
 #H_release = release(pH_guess, ["H3PO4", "H2PO4 -", "HPO4 2-", "PO4 3-"], [0.1, 0.5, 0.2, 0.3], [2, 7, 12], [3, 2, 1, 0])
 #pH_calc(H_release)
